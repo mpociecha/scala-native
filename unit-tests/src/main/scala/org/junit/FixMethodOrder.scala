529e9bde1607489286c7052e7fd158e70b61063b
Denys Shabalin <denys.shabalin@epfl.ch>
2016-09-06 15:20:25 +0200

    Port JUnit runtime from Scala.js

---
+++ unit-tests/src/main/scala/org/junit/FixMethodOrder.scala
@@ -0,0 +1,15 @@
+/*
+ * Ported from https://github.com/junit-team/junit
+ */
+package org.junit
+
+import java.lang.annotation._
+
+import org.junit.runners.MethodSorters
+
+class FixMethodOrder(val value: MethodSorters) extends Annotation {
+
+  def this() = this(MethodSorters.DEFAULT)
+
+  def annotationType(): Class[_ <: Annotation] = classOf[FixMethodOrder]
+}
